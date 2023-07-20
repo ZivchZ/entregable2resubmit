ac4047bf612a9fd7862e925a0ac9bb04c23290d3
Zivch <santiagoandreh022@gmail.com>
2023-07-20 18:45:04 -0500

    upload changes

--- src/components/Loading.jsx
+++ src/components/Loading.jsx
@@ -4,7 +4,7 @@ const Loading = () => {
     return(
         
         <div className="loading">
-            <img src='/assets/icons/7.svg'/>
+            <img src='/icons/7.svg'/>
             <h2>Cargando Aplicación...</h2>
         </div>
     )
--- src/components/Weather.jsx
+++ src/components/Weather.jsx
@@ -18,10 +18,10 @@ const Weather = ({url, isOn}) => {
             <li className="main-list">
                 {   
                     celsius < 20 
-                    ? <img src="./src/assets/icons/7.svg" alt="weather-img" />
+                    ? <img src="/icons/7.svg" alt="weather-img" />
                     : celsius < 35 
-                    ? <img src="./src/assets/icons/2.svg" alt="weather-img" />
-                    : <img src="./src/assets/icons/1.svg" alt="weather-img" />
+                    ? <img src="/icons/2.svg" alt="weather-img" />
+                    : <img src="/icons/1.svg" alt="weather-img" />
                 }
                 <h2>{`${changeGrade === true ? celsius : farenheit}°`}</h2>
                 <p>Viento: {url.wind?.speed}</p>
